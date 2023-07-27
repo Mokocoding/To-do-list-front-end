0889a03b40cd6ed72c10a40d3f9c1cd532367848
jaegyun0320 <zkdbqhdb@naver.com>
2023-07-27 11:28:56 +0900

    jaegyun : 메인페이지 기본레이아웃 작업

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,26 @@
-export default function Main() {
+import "./Main.css";
+
+ 
+function Main() {
+
+  
   return (
-    <div>
-      <h1>하이</h1>
-    </div>
+    <>
+     
+        <div className="mainBox">
+          <div className="title">To do list입니다</div>       
+        </div>
+        <div className="postBox">
+          <p>
+            to do calender
+          </p>
+          <input type="date" max="2023-12-31" className="date"></input>
+          <div className="postList">투두리스트목록</div>
+        </div>
+        <div className="createPost">투두리스트작성</div>
+         
+    </>
   );
 }
+
+export default Main
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Main from "./Main";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
