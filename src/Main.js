4aa67b5ea948d5f641b4d7a4136f9a8613b14a1f
jaegyun0320 <zkdbqhdb@naver.com>
2023-07-28 14:37:37 +0900

    jaegyun : PULL떙기기전 파일저장 변동x

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,17 @@
-import "./Main.css";
 
+import Users from "./Users";
+import Delete from "./Delete"
  
 function Main() {
 
   
   return (
     <>
-     
-        <div className="mainBox">
-          <div className="title">To do list입니다</div>       
-        </div>
-        <div className="postBox">
-          <p>
-            to do calender
-          </p>
-          <input type="date" max="2023-12-31" className="date"></input>
-          <div className="postList">투두리스트목록</div>
-        </div>
-        <div className="createPost">투두리스트작성</div>
+
+        <Users />
+        <Delete />
+        
+       
          
     </>
   );
