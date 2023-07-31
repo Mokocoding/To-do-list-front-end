1c04c63c0bfc356b195f1cd6f7fbd2f65e8b6b3d
박현우 <pho9902@naver.com>
2023-07-31 15:29:31 +0900

    for LEE

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import MainHead from "./components/MainHead";
 import MainBox from "./components/MainBox";
 import styled from 'styled-components';
+import axios from "axios";
 
 
 const StyledLink = styled(Link)`
@@ -21,10 +22,29 @@ const StyledLink = styled(Link)`
 
 
 export default function Main() {
+  
+function getPosts(method) {
+  const url = 'http://3.35.134.247:3000/api/posts'
+  if (method === 'post'){
+  axios.post(url,
+    {
+    descrption: 'test',
+    target_date : '2023-07-31'
+  }).then(res => console.log(res))
+  .catch(err => console.log(err))
+return null
+}
+axios.get(url).then(res => console.log(res)).catch(err => console.log(err))
+
+}
+
+
   return (
     <div>
       <MainHead />
       <StyledLink to="/post">할일 등록하러 가기</StyledLink>
+      <button onClick={() => getPosts('get')}>get테스트</button>
+      <button onClick={() => getPosts('post')}>post테스트</button>
       <MainBox />
      
     </div>
